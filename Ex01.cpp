02265bc99e128766eba396bdb0f69bf6daa1eb15
Ricardo Naoto Miyagi <naotomiy@gmail.com>
2017-09-16 17:27:41 -0300

    teste

---
+++ Ex01.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+  unsigned short int A, B, Soma;
+  printf("Tamanho da variaveis int = %d\n\n", sizeof(unsigned short int));
+  printf("Digite A: ");
+  scanf("%hu", &A);
+  printf("Digite B: ");
+  scanf("%hu", &B);
+  Soma = A + B;
+  printf("Soma de %hu com %hu = %hu", A, B, Soma);
+}
